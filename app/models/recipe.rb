28f5fb8c3e29850b88efbf3a790e17323bb1869f
pete3249 <katelynrpeterson16@gmail.com>
2021-01-07 17:40:02 -0500

    Add associations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
     has_many :recipe_moods
     has_many :moods, through: :recipe_moods
+    has_many :recipe_ingredients
+    has_many :ingredients, through: :recipe_ingredients
 end
