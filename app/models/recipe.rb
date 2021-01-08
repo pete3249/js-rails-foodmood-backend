5ad0bd8c032b3c50aef42635d43cf4b077555f8c
pete3249 <katelynrpeterson16@gmail.com>
2021-01-08 15:58:09 -0500

    Add method for filtering based on preferences

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,15 @@ class Recipe < ApplicationRecord
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
     has_many :comments
+
+    def self.get_recipes(days, moods)
+       if moods == "null"
+            filtered_recipes = Recipe.all
+       else
+            mood_array = moods.split(",")
+            filtered_recipes = joins(:recipe_moods).where(recipe_moods: {mood_id: mood_array})
+       end 
+       selected_recipes = filtered_recipes.sample(days.to_i)
+    end 
+
 end
