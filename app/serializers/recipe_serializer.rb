1edf61b163dec704f483cbb592f2a1c619316035
pete3249 <katelynrpeterson16@gmail.com>
2021-01-10 19:57:29 -0500

    Remove unneaded info from serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -3,12 +3,6 @@ class RecipeSerializer
 
   attributes :id, :name, :author, :description, :image_url, :instructions, :recipe_ingredients
 
-  attribute :instructions do |recipe|
-    recipe.instructions.map do |instruction|
-      "#{instruction}"
-    end
-  end 
-
   attribute :recipe_ingredients do |recipe|
       recipe.recipe_ingredients.map do |recipe_ingredient|
         { amount: "#{recipe_ingredient.amount}", name: "#{recipe_ingredient.ingredient.name}", notes: "#{recipe_ingredient.notes}" }
