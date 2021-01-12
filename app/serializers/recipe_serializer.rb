33d220e3ce29e60f040e2d793b8f1b265bb8cbe1
pete3249 <katelynrpeterson16@gmail.com>
2021-01-12 00:58:33 -0500

    Add custom attribute method for comments

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,12 +1,20 @@
 class RecipeSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :name, :author, :description, :image_url, :instructions, :recipe_ingredients
+  attributes :id, :name, :author, :description, :image_url, :instructions, :recipe_ingredients, :comments
 
   attribute :recipe_ingredients do |recipe|
       recipe.recipe_ingredients.map do |recipe_ingredient|
         { amount: "#{recipe_ingredient.amount}", name: "#{recipe_ingredient.ingredient.name}", notes: "#{recipe_ingredient.notes}" }
       end
+  end 
+
+  attribute :comments do |recipe|
+    if recipe.comments.length > 0
+      recipe.comments.map do |comment|
+        { name: "#{comment.name}", review: "#{comment.review}", recipe_id: "#{comment.recipe_id}", date: "#{comment.created_at.strftime("%B %Y")}"}
+      end 
+    end 
   end 
 
 end 
