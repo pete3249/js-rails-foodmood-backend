f50bdd1dcaa0d54fde8a4200f0513a9e98cae4eb
pete3249 <katelynrpeterson16@gmail.com>
2021-01-12 16:17:39 -0500

    add recipe_id to serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,6 @@
 class CommentSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :review, :created_at
+  attributes :id, :name, :review, :recipe_id, :created_at
 
   attribute :created_at do |comment|
     "#{comment.created_at.strftime("%B %Y")}"
